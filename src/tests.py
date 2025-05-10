055d40009f909534467988a501facf22833364a2
nexy <nexyvanessa@gmail.com>
2025-05-10 19:59:59 +0200

    extend maze initialization test

--- src/tests.py
+++ src/tests.py
@@ -3,17 +3,21 @@ from maze import Maze
 
 class Tests(unittest.TestCase):
     def test_maze_create_cells(self):
-        num_cols = 12
-        num_rows = 10
-        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
-        self.assertEqual(
-            len(m1.maze),
-            num_rows,
-        )
-        self.assertEqual(
-            len(m1.maze[0]),
-            num_cols,
-        )
+        test_sizes = [
+            [12, 10],
+            [1, 8],
+            [16, 16]
+        ]
+        for case_size in test_sizes:
+            m = Maze(0, 0, case_size[0], case_size[1], 10, 10)
+            self.assertEqual(
+                len(m.maze),
+                case_size[0],
+            )
+            self.assertEqual(
+                len(m.maze[0]),
+                case_size[1],
+            )
 
 if __name__ == "__main__":
     unittest.main()
