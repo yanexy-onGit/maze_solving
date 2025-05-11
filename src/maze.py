031d66638179dfecc9ee0abf788f91625dffa43d
nexy <nexyvanessa@gmail.com>
2025-05-11 11:35:48 +0200

    write out __mazefy and necessary helper functions

--- src/maze.py
+++ src/maze.py
@@ -1,9 +1,10 @@
 from strokes import Point, Line
 from time import sleep
-from random import seed, randint
+from random import seed, randint, shuffle
 
 class Maze:
     def __init__(self, x, y, rows_count, cols_count, cell_size_x, cell_size_y, window=None, seed=None):
+        self.__testing_fcall_count = 0
         self.x = x
         self.y = y
         self.rows_count = int(rows_count)
@@ -15,7 +16,17 @@ class Maze:
         self.maze = [[None]*self.cols_count for _ in range(self.rows_count)]
         self.__create_cells()
         self.__break_entrance_and_exit()
-        print(self.__cross((0, 4), "right"))
+        self.__mazefy()
+
+    # ? discard
+    def reset(self):
+        self.maze = [[None]*self.cols_count for _ in range(self.rows_count)]
+        self.__create_cells()
+
+    def cell_at(self, coordinates):
+        if not self.__coordinates_valid(*coordinates):
+            return None
+        return self.maze[coordinates[0]][coordinates[-1]]
         
     # tested!
     def __break_entrance_and_exit(self):
@@ -30,8 +41,7 @@ class Maze:
         return False
 
     # tested!
-    def __cross(self, coordinates, direction):
-        row_idx, col_idx = coordinates
+    def __cross_by_direction(self, coordinates, direction):
         if type(direction) == str:
             idx_dict = { "l": 0, "u": 1, "r": 2, "d": 3 }
             break_at_idx= idx_dict.get(direction[0], None)
@@ -40,18 +50,83 @@ class Maze:
         if break_at_idx == None:
             raise ValueError("invalid direction")
         if self.__coordinates_valid(*coordinates):
+            row_idx, col_idx = coordinates
             new_coordinates = [(row_idx, col_idx-1), (row_idx-1, col_idx), (row_idx, col_idx+1), (row_idx+1, col_idx)][break_at_idx]
         if self.__coordinates_valid(*new_coordinates):
-            self.maze[row_idx][col_idx].toggle_wall(break_at_idx)
-            new_row_idx, new_col_idx = new_coordinates
-            self.maze[new_row_idx][new_col_idx].toggle_wall((break_at_idx+2)%4)
+            self.cell_at(coordinates).toggle_wall(break_at_idx)
+            self.cell_at(new_coordinates).toggle_wall((break_at_idx+2)%4)
             return new_coordinates
         return None
+    
+    # ? test
+    def __cross(self, coords_from, coords_to):
+        row_idx, col_idx = coords_from
+        direction = [(row_idx, col_idx-1), (row_idx-1, col_idx), (row_idx, col_idx+1), (row_idx+1, col_idx), coords_to].index(coords_to)
+        # testing_dict = ["left", "up", "right", "down"]
+        # print(f"__cross-ing from {coords_from} in direction {testing_dict[direction]}")
+        if direction > 3:
+            raise Exception("cells to be crossed between must be adjacent")
+        return self.__cross_by_direction(coords_from, direction=direction)
+
+    def __undo_cross(self, coords_from, coords_to):
+        pass
+
+    # ? test
+    def cell_adjacents(self, coordinates, ignore_visited=False):
+        row_idx, col_idx = coordinates
+        adjacents = [coords for coords in [(row_idx, col_idx-1), (row_idx-1, col_idx), (row_idx, col_idx+1), (row_idx+1, col_idx)] if self.__coordinates_valid(*coords)]
+        if ignore_visited:
+            adjacents = [coords for coords in adjacents if not self.cell_at(coords).visited]
+        return adjacents
+
+    # ? test
+    def create_solution_from(self, coordinates=(0, 0)):
+        if coordinates == (self.rows_count-1, self.cols_count-1):
+            return 0
+        adjacents = self.cell_adjacents(coordinates, ignore_visited=True)
+        print(f"{coordinates} adjacents are: {adjacents}")
+        if not adjacents:
+            return -1
+        shuffle(adjacents)
+        for adjace_coords in adjacents:
+            self.__cross(coordinates, adjace_coords)
+            res = self.create_solution_from(adjace_coords)
+            if res == 0:
+                return 0
+            # self.cell_at(adjace_coords).reset_walls()
+            # self.cell_at(adjace_coords).set_unvisited()
+            # self.cell_at(coordinates).reset_walls()
+        return -2
+    
+    # ? test
+    def scramble(self):
+        unvisited = []
+        for row_idx in range(self.rows_count):
+            for col_idx in range(self.cols_count):
+                coords = (row_idx, col_idx)
+                self.cell_at(coords).visited or unvisited.append(coords)
+        while len(unvisited) > 1:
+            coords = unvisited.pop()
+            adjacents = self.cell_adjacents(coords)
+            shuffle(adjacents)
+            adjacents = adjacents[:2]
+            unvisited = [unvis_coords for unvis_coords in unvisited if not unvis_coords in adjacents]
+            for adjace_coords in adjacents:
+                self.__cross(coords, adjace_coords)
+        if unvisited:
+            adjacents = self.cell_adjacents(unvisited[0])
+            shuffle(adjacents)
+            self.__cross(unvisited[0], adjacents[0])
+
+
 
     # ? test
     def __mazefy(self):
         if self.__seed != None:
             seed(self.__seed)
+        res = self.create_solution_from((0, 0))
+        print(res)
+        self.scramble()
         
 
     def __create_cells(self):
@@ -82,7 +157,6 @@ class Maze:
 
 
 
-
 class Cell:
     __size = 10
     def __init__(self, x, y, window=None, size=0):
@@ -105,8 +179,7 @@ class Cell:
             return
         for i in range(4):
             clr = self.has_wall_left_clockwise[i] and "black" or "#d9d9d9"
-            self.__window.draw_line(self.__wall_lines[i], clr)
-                
+            self.__window.draw_line(self.__wall_lines[i], clr)            
     
     def toggle_wall(self, *keys, visit=True):
         keys = set(keys)
@@ -122,6 +195,13 @@ class Cell:
         if visit:
             self.visited = True
         self.draw()
+
+    def reset_walls(self):
+        self.has_wall_left_clockwise = [True] * 4
+        self.draw()
+    
+    def set_unvisited(self):
+        self.visited = False
 
     def __get_centre_p(self):
         return Point(self.__x+.5*self.size, self.__y+.5*self.size)
